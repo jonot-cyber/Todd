c1e6fdbd03dc41c49eab21fae748de232e459554
Joseph Tessitore <josephtessitore@protonmail.com>
2024-01-31 12:33:45 -0500

    Split Multiboot information into a different file
    It doesn't really make sense for this to be in common.

--- src/common.c
+++ src/common.c
@@ -58,19 +58,3 @@ void usleep(u32 ms) {
 	}
 	return;
 }
-
-bool multiboot_mem_present(struct MultiBoot* m) {
-	return m->flags & (1 << 0);
-}
-
-bool multiboot_boot_device_present(struct MultiBoot* m) {
-	return m->flags & (1 << 1);
-}
-
-bool multiboot_cmd_line_present(struct MultiBoot* m) {
-	return m->flags & (1 << 2);
-}
-
-bool multiboot_mods_present(struct MultiBoot* m) {
-	return m->flags & (1 << 3);
-}
