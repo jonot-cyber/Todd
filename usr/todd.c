904bed3c2eefdfdaae8feadb1995d3286e9054e4
Joseph Tessitore <josephtessitore@protonmail.com>
2024-04-26 11:34:14 -0400

    I have done this before???
    Anyways fix this

--- usr/todd.c
+++ usr/todd.c
@@ -1,8 +1,8 @@
 #include "system.h"
 
-static char *todd = "   ___\n__/.  \\    ,\n\\_____/___/|\n  /        |\n  \\_______/\n";
 
 int main() {
+	char *todd = "   ___\n__/.  \\    ,\n\\_____/___/|\n  /        |\n  \\_______/\n";
 	write(todd, 58);
 	return 0;
 }
